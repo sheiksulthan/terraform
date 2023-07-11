3d2ee14de223b0eaebc5585801962666a980e322
Shaick Sulthan H <58335934+sheiksulthan@users.noreply.github.com>
2023-07-11 23:05:19 +0530

    Update instance.tf
    updated the volume.

--- Create EC2 with new vpc/instance.tf
+++ Create EC2 with new vpc/instance.tf
@@ -7,54 +7,37 @@ key_name                    = var.key_name
 security_groups             = ["${aws_security_group.SSH-security-group.id}"]
 subnet_id                   = "${aws_subnet.public-subnet.id}"
 associate_public_ip_address = true
+disable_api_termination		= false
 lifecycle {
 create_before_destroy = true
 }
 tags = {
-"Name" = "EC2-PUBLIC"
+"Name" = "EC2-Public"
 }
 }
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1a"
+  size              = 10
+  encrypted			= true
 
-#user_data                   = "${data.template_file.provision.rendered}"
-#iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-
-lifecycle {
-create_before_destroy = true
+  tags = {
+    Name = "ec2 public"
+  }
 }
-
-# Copies the ssh key file to home dir
-# Copies the ssh key file to home dir
-#provisioner "file" {
-#source      = "./${var.key_name}.pem"
-#destination = "/home/ec2-user/${var.key_name}.pem"
-#connection {
-#type        = "ssh"
-#user        = "ec2-user"
-#private_key = file("${var.key_name}.pem")
-#host        = self.public_ip
+#resource "aws_volume_attachment" "my ebs" {
+# device_name = "/dev/xvda1"
+#  volume_id   = aws_ebs_volume.example.id
+# instance_id = aws_instance.ec2_public.id
 #}
-#}
-#//chmod key 400 on EC2 instance
-#provisioner "remote-exec" {
-#inline = ["chmod 400 ~/${var.key_name}.pem"]
-#connection {
-#type        = "ssh"
-#user        = "ec2-user"
-#private_key = file("${var.key_name}.pem")
-#host        = self.public_ip
-#}
-#}
-#}
-
 #Create a new EC2 launch configuration
 resource "aws_instance" "ec2_private" {
-#name_prefix                 = "terraform-example-web-instance"
 ami                    = "ami-03e7d35abf8845efc"
 instance_type               = var.instance_type
 key_name                    = var.key_name
 security_groups             = ["${aws_security_group.webserver-security-group.id}"]
 subnet_id                   = "${aws_subnet.private-subnet.id}"
 associate_public_ip_address = false
+disable_api_termination		= false
 
 #user_data                   = "${data.template_file.provision.rendered}"
 #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
@@ -62,6 +45,14 @@ lifecycle {
 create_before_destroy = true
 }
 tags = {
-"Name" = "EC2-PUBLIC"
+"Name" = "EC2-Private"
 }
+}
+resource "aws_ebs_volume" "example2" {
+  availability_zone = "us-east-1b"
+  size              = 10
+
+  tags = {
+    Name = "ec2 private"
+  }
 }
